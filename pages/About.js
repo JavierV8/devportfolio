b32705e1eb9d85e4b9814b5453058b7f97428cd6
JavSan <javier.san.mail@gmail.com>
2020-02-19 18:53:53 +0100

    about page: creating format and style of about page.

--- pages/About.js
+++ pages/About.js
@@ -6,7 +6,46 @@ const About = props => {
   const { auth } = props;
   return (
     <BaseLayout auth={auth} className="cover">
-      <BasePage className="about-page" title="About">
+      <BasePage className="about-page">
+        <div className="about-section">
+          <div className="left-side bounceInLeft">
+            <img className="image-About" src='../static/images/cara.png'></img>
+          </div>
+          <div className="right-side bounceInRight">
+            <h1 className="about-title">About me</h1>
+            <h5 className="about-subtitle">My name is Javier Sanchez and I am an experienced software engineer.</h5>
+            <div className="about-image-arround">
+              <p className="about-subsubTitle">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+                a galley of type and scrambled it to make a type specimen book. It has survived not only
+                five centuries, but also the leap into electronic typesetting, remaining essentially
+                unchanged.
+              </p>
+              <img className="image-About-2" src='../static/images/cara.png'></img>
+            </div>
+
+            <p className="about-subsubTitle">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+              been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+              a galley of type and scrambled it to make a type specimen book. It has survived not only
+              five centuries, but also the leap into electronic typesetting, remaining essentially
+              unchanged.
+            </p>
+            <p className="about-subsubTitle">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+              been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+              a galley of type and scrambled it to make a type specimen book. It has survived not only
+              five centuries, but also the leap into electronic typesetting, remaining essentially
+              unchanged.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+              been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+              a galley of type and scrambled it to make a type specimen book. It has survived not only
+              five centuries, but also the leap into electronic typesetting, remaining essentially
+              unchanged.
+          </p>
+          </div>
+        </div>
       </BasePage>
     </BaseLayout>
   )
