0e95ff6a3f4e4abd15f44ca164267f517aba0a2e
JavSan <javier.san.mail@gmail.com>
2020-03-02 17:27:51 +0100

    IndexCarrousel: adding carrousel animation for index page

---
+++ components/Word_Carousel/Word_Carousel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Word_Carousel = () => {
+    return (
+        <div class="carousel-word">
+            <div class="pre"> </div>
+            <div class="change_outer">
+                <div class="change_inner">
+                    <h1 class="element text-index-welcome-3">Software Engineer</h1>
+                    <h1 class="element text-index-welcome-3">Software Enthusiast</h1>
+                    <h1 class="element text-index-welcome-3">Control Systems Engineer</h1>
+                    <h1 class="element text-index-welcome-3">Front-End Developer</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Word_Carousel;
