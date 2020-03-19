1cd20d4186f960b4c7bac7d5a9b3f9dc289d171b
javier S <javier.san.mail@gmail.com>
2020-03-19 21:20:44 +0100

    server index changes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const next = require('next');
-const routes = require('../routes');
-
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
+const routes = require('../routes');
 const handle = routes.getRequestHandler(app);
 const server = express();
+const cors = require('cors');
 
-var cors = require('cors');
 server.use(cors());
 server.options('*', cors());
+
 require('./startup/logging')();
 require('./startup/routes')(server);
 require('./startup/db')();
@@ -19,18 +19,14 @@ require('./startup/prod')(server);
 
 app.prepare()
     .then(() => {
-        //const server = express();
-
         server.get('*', (req, res) => {
-            return handle(req, res)
+            return handle(req, res);
         });
-
         const PORT = process.env.PORT || 4000;
         server.use(handle).listen(PORT, (err) => {
             if (err) throw err;
             console.log('> Ready on port ' + PORT);
         })
-
     })
     .catch((ex) => {
         console.error(ex.stack)
