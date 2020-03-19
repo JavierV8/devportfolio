390410db5f95f46553c0b86ffea879c1e265f7df
javier S <javier.san.mail@gmail.com>
2020-03-19 21:30:28 +0100

    update routes file

--- routes.js
+++ routes.js
@@ -1,6 +1,4 @@
 const routes = require('next-routes');
 
-module.exports = routes()
-    .add('portafolioNew', '/portfolios/new')
-    .add('portfolio', '/portfolio/:id')
-    .add('portfolioEdit', '/portfolios/:id/edit')
+module.exports = routes();
+
