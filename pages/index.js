db9c595efa7f3859dd5159ae7ee1eceebee7d2c5
javier <javier.san.mail@gmail.com>
2020-04-15 12:25:08 +0200

    some changes on pages

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,11 @@ const Index = props => {
                 document.getElementById("image-index-id").style.width = (widhtScreen) + "px";
             }
         } else {
-            let imageWidht = (((window.innerWidth / 2) * 85 / 100) / 6);
+            let porcentage = (window.innerWidth * 5.5) / 100
+            let imageWidht = (((window.innerWidth / 2 - porcentage) * 100 / 100) / 6);
             setImageWdith(imageWidht - 1)
             const widthBox = (window.innerWidth / 2) * 0.9;
-            const porcent = (widthBox * 80) / 100;
+            const porcent = (widthBox * 85) / 100;
             document.getElementById("image-index-id").style.height = porcent + "px";
         }
     }
@@ -41,10 +42,12 @@ const Index = props => {
     useEffect(() => {
         window.addEventListener('resize', resizeFunction);
         resizeFunction();
+        pageloaded();
         return () => {
             window.removeEventListener('resize', resizeFunction);
         };
     }, [resizeFunction]);
+
     const redirectPage = () => {
         Router.push('/about');
     }
@@ -52,34 +55,30 @@ const Index = props => {
     let imageStyle = [];
     let random1 = null;
     let random2 = null;
-    let images1 = [];
     let images = [];
     if (isIniciated) {
-        for (let i = 1; i < 38; i++) (
+        for (let i = 0; i <= 36; i++) (
             random1 = Math.floor(Math.random() * (600 - -600 + 1)) + -600,
             random2 = Math.floor(Math.random() * (600 - -600 + 1)) + -600,
-            imageStyle.push({ transform: `translate(${random1}px, ${random2}px)` })
+            imageStyle.push({ transform: `translate(${random1}px, ${random2}px)`, minWidth: imageWdith - 5 })
         )
     } else {
-        for (let i = 1; i < 38; i++) (
+        for (let i = 0; i <= 36; i++) (
             imageStyle.push({ transform: `0px`, minWidth: imageWdith - 5 })
         )
     }
-    for (let i = 1; i < 10; i++) (
-        images1.push(<img style={imageStyle[i]} alt="resized image" id={`image_${i}`} className="imageCara" src={`../static/images/cara/Index_0${i}.jpg`} />)
-    )
-    for (let i = 10; i < 37; i++) (
-        images.push(<img style={imageStyle[i]} alt="resized image" id={`image_${i}`} className="imageCara" src={`../static/images/cara/Index_${i}.jpg`} />)
+
+    for (let i = 1; i <= 36; i++) (
+        images.push(<img style={imageStyle[i]} alt="resized image" id={`image_${i}`} className="imageCara" src={`../static/images/cara/Index_${i}.png`} />)
     )
 
     const pageloaded = () => {
-
         setIsIniciated(false)
         setTimeout(function () {
-            for (let i = 1; i < 37; i++) (
+            for (let i = 1; i <= 36; i++) (
                 document.getElementById(`image_${i}`).style.transform = `translate(${0}px, ${0}px)`
             )
-        }, 10);
+        }, 100);
         document.getElementById("main-section-id").style.display = "flex";
         document.getElementById("image-index-id").style.display = "inline";
         document.getElementById("text-index-id").style.display = "inline";
@@ -87,15 +86,15 @@ const Index = props => {
 
     let render = null;
     isIniciated ? render = <Spinner /> : render = null;
+
     return (
         <BaseLayout auth={auth} headerType="index">
             {render}
-            <div className="main-section" id="main-section-id">
-                <div md="6" className="image-index" id="image-index-id">
-                    {images1}
+            <div className="main-section2" id="main-section-id">
+                <div className="image-index" id="image-index-id">
                     {images}
                 </div>
-                <div md="6" className="text-index" id="text-index-id">
+                <div className="text-index" id="text-index-id">
                     <div className="text-index-welcome" id="text-index-welcome-id">
                         <div className="text-index-welcome-1">Hi Im</div>
                         <div className="text-index-welcome-2">Javier Sanchez</div>
@@ -124,7 +123,7 @@ const Index = props => {
                     <Button className="index-about-button" onClick={redirectPage}>about me</Button>
                 </div>
                 <div className="index-footer">
-                    <img alt="resized image" onLoad={pageloaded} src='../static/images/logos/github.png' className="github-img" onClick={() => window.open("https://github.com/javierV8")}></img>
+                    <img alt="resized image" src='../static/images/logos/github.png' className="github-img" onClick={() => window.open("https://github.com/javierV8")}></img>
                 </div>
             </div>
         </BaseLayout>
