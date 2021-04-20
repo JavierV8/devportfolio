38238d0eb7dba37cc923c9ab025228e42a7f7c51
javier sanchez <javier.san.mail@gmail.com>
2021-04-20 17:22:02 +0200

    STAR CLEAN FOR REFACTOR

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,7 @@
+import '../styles/globals.css'
 
-import React from "react";
-import App, { Container } from "next/app";
-import { Provider } from "react-redux";
-import withRedux from "next-redux-wrapper";
-// Stylings
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/main.scss';
-
-import { initStore } from "../store/index";
-import * as actions from '../store/actions/index';
-
-class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
-        let isAuth, isAdmin;
-        process.browser ?
-            await ctx.store.dispatch(actions.clientAuth())
-            :
-            await ctx.store.dispatch(actions.serverAuth(ctx.req));
-        ctx.store.getState().auth.token != undefined ? isAuth = true : isAuth = false;
-        ctx.store.getState().auth.isAdmin != undefined ? isAdmin = true : isAdmin = false;
-        if (Component.getInitialProps) pageProps = await Component.getInitialProps(ctx);
-        const auth = { isAuth, isAdmin };
-        return { pageProps, auth };
-    }
-
-    render() {
-        const { Component, pageProps, store, auth } = this.props;
-        return (
-            <Container>
-                <Provider store={store}>
-                    <Component {...pageProps} auth={auth} />
-                </Provider>
-            </Container>
-        );
-    }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-export default withRedux(initStore)(MyApp);
+export default MyApp
--- store/actions/contact.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import * as actionType from './actionType';
-
-export const sendQuestion = async (payload) => {
-    return await axios.post(actionType.URL + 'contact', payload).then(response => response.data);
-};
