e8b52ddf2134326e515dacf97c8676b2d565d006
javier sanchez <javier.san.mail@gmail.com>
2021-05-16 14:19:11 +0200

    updated project file description

--- static/projects.js
+++ static/projects.js
@@ -1,75 +1,75 @@
 export const projects = [
     {
-        "title": "Canvas",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "canvas.png",
-        "demo_url": "https://canvas-f00be.web.app/",
-        "gitHub_url": "https://github.com/JavierV8/Canvas"
-    },
-    {
-        "title": "Hooks",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "hooks.png",
-        "demo_url": "https://react-hooks-e1106.web.app/",
-        "gitHub_url": "https://github.com/JavierV8/React-Hooks-functionalities"
-    },
-    {
-        "title": "Movie App",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "movieApp.png",
-        "demo_url": "https://moviesearch-91113.firebaseapp.com/",
-        "gitHub_url": "https://github.com/JavierV8/Movie-Search"
-    },
-    {
-        "title": "Puzzle",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "puzzle.png",
-        "demo_url": "https://puzzle-df319.web.app/",
-        "gitHub_url": "https://github.com/JavierV8/Puzzle"
-    },
-    {
-        "title": "Store App",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "storeApp.png",
-        "demo_url": "https://e-commerce-mernapp.herokuapp.com/#/",
-        "gitHub_url": "https://github.com/JavierV8/MERN-E-commerce"
-    },
-    {
-        "title": "Real Time Data Monitoring",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "dataMonitoring.png",
-        "demo_url": "https://real-time-data-monitoring.web.app/",
-        "gitHub_url": "https://github.com/JavierV8/Real-time-data-monitoring"
-    },
-    {
-        "title": "Web",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "web.png",
-        "demo_url": "",
-        "gitHub_url": "https://github.com/JavierV8/devportfolio"
-    },
-    {
-        "title": "Matress Store",
-        "subtitle": "Html, JavasScript, css",
-        "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
-        "image": "mattress.png",
-        "demo_url": "https://mattress-store-c4c36.web.app/#/",
-        "gitHub_url": "https://github.com/JavierV8/mattress-store"
-    },
-    {
         "title": "Snake Game",
         "subtitle": "Html, JavasScript, css",
         "text": "Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes ridiculus malesuada inceptos; mi orci per senectus magna. Inceptos nec feugiat turpis; neque scelerisque turpis mi. Potenti quam placerat a ex eleifend ridiculus vel mollis. Erat inceptos velit nulla condimentum nec, eu sodales. Aptent suscipit litora torquent fusce et inceptos. Justo urna tempor ullamcorper; turpis donec pellentesque commodo.",
         "image": "snake.png",
         "demo_url": "https://snake-game-a7d1b.web.app/",
         "gitHub_url": "https://github.com/JavierV8/SnakeGame"
-    }
+    },
+    {
+        "title": "Matress Store",
+        "subtitle": "ES6 | Bootstrap | Reactstrap | Express | React | Redux | MongoDB",
+        "text": "This web app is a small representation of a mattress store with small API that provide all the data to the front made in React.",
+        "image": "mattress.png",
+        "demo_url": "https://mattress-store-c4c36.web.app/#/",
+        "gitHub_url": "https://github.com/JavierV8/mattress-store"
+    },
+    {
+        "title": "Hooks",
+        "subtitle": "JavaScript | React | React-Hooks",
+        "text": "Example project with all new React Hooks functionality: useState, useRedux, useEffect, useContext, useCallback, useMemo, useRef and custom Hooks.",
+        "image": "hooks.png",
+        "demo_url": "https://react-hooks-e1106.web.app/",
+        "gitHub_url": "https://github.com/JavierV8/React-Hooks-functionalities"
+    },
+    {
+        "title": "Web",
+        "subtitle": "JavaScript | React | NextJS",
+        "text": "This is my portfolio web, made with nextJS.",
+        "image": "web.png",
+        "demo_url": "",
+        "gitHub_url": "https://github.com/JavierV8/devportfolio"
+    },
+    {
+        "title": "Real Time Data Monitoring",
+        "subtitle": "ES6 | Arduino | C++ | MQTT | WebSockets | Chart.js | Bootstrap",
+        "text": "This is a Demo for monitoring data from an Arduino that send the temperature and humidity from a sensor conected to it. The data is sending to a NodeJS server through MQTT. The client that represent the data in a web APP in a very beatufully way is conected to the server by websockets so it allow to show the data in real time.",
+        "image": "dataMonitoring.png",
+        "demo_url": "https://real-time-data-monitoring.web.app/",
+        "gitHub_url": "https://github.com/JavierV8/Real-time-data-monitoring"
+    },
+    {
+        "title": "Store App",
+        "subtitle": "React | Redux | NodeJS | Express | MongoDB | JWT",
+        "text": "Ecommerce online shop is a full-stack MERN web App. This project shows how to build an entire app with just javascript, powered with React and express. You can create account make a shop purchase and watch all your purchse history.",
+        "image": "storeApp.png",
+        "demo_url": "https://e-commerce-mernapp.herokuapp.com/#/",
+        "gitHub_url": "https://github.com/JavierV8/MERN-E-commerce"
+    },
+    {
+        "title": "Movie App",
+        "subtitle": "React | Redux | Router | WebPack | Axios",
+        "text": "Movie-Search is a responsive React app that load data from The Movie Database API. This simple project shows how React components and Redux can be used to build a friendly user experience with instant visual updates and scaleable code.",
+        "image": "movieApp.png",
+        "demo_url": "https://moviesearch-91113.firebaseapp.com/",
+        "gitHub_url": "https://github.com/JavierV8/Movie-Search"
+    },
+    {
+        "title": "Canvas",
+        "subtitle": "Html | JavasScript | CSS | Canvas",
+        "text": "The canvas animation is just a canvas with a control panel where you can choose one of the functionalities it provided and paint something on the canvas, but be aware that some of the functionallities have some animations.",
+        "image": "canvas.png",
+        "demo_url": "https://canvas-f00be.web.app/",
+        "gitHub_url": "https://github.com/JavierV8/Canvas"
+    },
+    {
+        "title": "Puzzle",
+        "subtitle": "Html | JavasScript | CSS",
+        "text": "This is an interactive puzzle made with vanilla JavaScript. The pieces of the puzzle can be increased or decreased to make it easier or more complicated.",
+        "image": "puzzle.png",
+        "demo_url": "https://puzzle-df319.web.app/",
+        "gitHub_url": "https://github.com/JavierV8/Puzzle"
+    },
     
 ]
